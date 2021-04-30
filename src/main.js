90e12fd0b3d44e34a92b1d78ab841369db4e8e79
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-30 01:55:27 -0400

    Add some styling to the project

--- src/main.js
+++ src/main.js
@@ -5,20 +5,24 @@ import './css/styles.css';
 import CurrencyExchangeService from "./js/currencyExchangeService";
 
 function getElements(response, amount) {
-  if (response.base_code != undefined && amount > 0) {
+  if (response.base_code != undefined && amount > 0 && Number(amount)) {
     $('#output').text(`${amount} ${response.base_code} is ${response.conversion_rate * amount} in ${response.target_code}`);
   } else {
     $('#output').text(`Error: there was error processing you request.`);
   }
   if (response.base_code == undefined || response.target_code == undefined) {
     $('#base').text(`Error: Please enter a valid currency key.`);
+    $('#base').show();
   }
   if (amount < 0) {
     $('#nonegative').text(`Error: Please enter a positive number.`);
+    $('#nonegative').show();
   } else if (amount == "") {
     $('#space').text(`Error: Amount input value is required before submiting.`);
+    $('#space').show();
   } else if (!Number(amount)) {
     $('#nan').text(`Error: Amount input supports only positive numbers.`);
+    $('#nan').show("");
   }
 }
 
@@ -29,6 +33,7 @@ async function makeApiCall(base_code, amount, other_code) {
 
 $(document).ready(function () {
   $('#result').click(function () {
+    $('#output').show();
     let base_code = $('#userInputBaseCurrency').val();
     let amount = $('#userInputAmount').val();
     let other_code = $("#userInputOtherCurrency").val();
@@ -40,9 +45,9 @@ $(document).ready(function () {
     makeApiCall(base_code, amount, other_code);
 
     $('#output').text("");
-    $('#base').text("");
-    $('#nonegative').text("");
-    $('#space').text("");
-    $('#nan').text("");
+    $('#base').hide("");
+    $('#nonegative').hide("");
+    $('#space').hide("");
+    $('#nan').hide("");
   });
 });
